ea98b812854c6e64d5cf1413430d75c90136439d
unknown <kingdavz>
2025-08-27 14:31:18 +1100

    a python file

---
+++ 0x00/4-operators.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+"""A module that performs arithmetic operations"""
+num1 = float(input("Enter your first number:"))
+num2 = float(input("Enter your second number:"))
+print("arithmetic operations")
+print(f"Addition {num1+num2}")
+print(f"Subtraction {num1-num2}")
+print(f"Multiplication {num1*num2}")
+print(f"division {num1/num2}")
+print(f"Exponential {num1**num2}")
+print(f"modulus {num1%num2}")
+print(f"Division {num1//num2}")
