afd1c3635c02d513b464887df0b035eae899f367
unknown <kingdavz>
2025-08-28 13:59:24 +1100

    Add conditional statements module for age verification

---
+++ 0x01/1-ifelse.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+"""A module that handles conditional statment"""
+age = int(input("Enter your age:"))
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are a child")
+    
