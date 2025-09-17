caae2311890b0bf644e0d3d0af62aca8f454c31c
unknown <kingdavz>
2025-09-17 13:06:41 +1100

    a module that performs indexing on a string

---
+++ 0x03/12-strpos.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+"""A module that performs indexing on a string"""
+
+message = "Python programming is awesome"
+
+
+print(message[0:18])
---
+++ 0x03/13-message.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+"""a module that performs indexing on a srting"""
+
+message = "python programming is awesome"
+
+combined = message[0:6] + message[18:29]
+print(combined)
